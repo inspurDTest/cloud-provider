1120826cb647dcc3d7ec8424e330ca7534a1364a
wangyd1988 <wang_yudong@inspur.com>
2023-12-25 15:52:10 +0800

    init

--- controllers/endpointslice/config/v1alpha1/zz_generated.conversion.go
+++ controllers/endpointslice/config/v1alpha1/zz_generated.conversion.go
@@ -24,7 +24,7 @@ package v1alpha1
 import (
 	conversion "k8s.io/apimachinery/pkg/conversion"
 	runtime "k8s.io/apimachinery/pkg/runtime"
-	config "github.com/inspurDTest/cloud-provider/controllers/endpointSlice/config"
+	config "github.com/inspurDTest/cloud-provider/controllers/endpointslice/config"
 )
 
 func init() {
